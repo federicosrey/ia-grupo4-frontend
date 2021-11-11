4a24562db3570c5c16bfacfa43efc626fba19011
Federico Rey <federico.s.rey@gmail.com>
2021-11-10 21:20:00 -0300

    cached

--- src/controller/constants.js
+++
@@ -1,5 +0,0 @@
-//URL Backend  
-
-//export const urlApi = process.env.REACT_APP_URL || "http://localhost:4000/";
-//URL Heroku
-export const urlApi = process.env.REACT_APP_URL || "https://ia-grupo4-backend.herokuapp.com/";
